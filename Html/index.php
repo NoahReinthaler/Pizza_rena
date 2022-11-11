cdf747f5409540556b7e72c3d73d27be58de08ee
Noah Reinthaler <noah.reinthaler@tfs-haslach.at>
2022-11-11 11:35:47 +0100

    Link-tree

--- Html/index.php
+++ Html/index.php
@@ -4,11 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="/CSS/sytle.css">
+    <title>Pizza_ReNa</title>
 </head>
 <body>
-    <h1>Pizza Rena for live</h1>
-        <h1>Hallo</h1>
-        <h2>Yeah Guys this is my 10 follower special here on youtube today im showing you my minecraft world yeah guys</h2>
+
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="menue.php">Menü</a></li>
+        <li><a href="karriere.php">Karriere</a></li>
+        <li><a href="ueberuns.php">Über uns</a></li>
+        <li><a href="kontakt.php">Karriere</a></li>
+    </ul>
+        
+        
+        
 </body>
 </html>
