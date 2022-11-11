90f35a675399c564572ee62124f3bf8452b97e1b
Noah Reinthaler <noah.reinthaler@tfs-haslach.at>
2022-11-11 11:57:54 +0100

    linktree

--- Html/index.php
+++ Html/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/CSS/sytle.css">
+    <link rel="stylesheet" href="../CSS/sytle.css">
     <title>Pizza_ReNa</title>
 </head >
 <body>
@@ -17,6 +17,7 @@
         <li><a class="linktree" href="kontakt.php">Karriere</a></li>
     </ul>
         <h1>Das sind unsere Beliebtesten Pizzen</h1>
+    
 
         
         
