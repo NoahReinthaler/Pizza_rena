fdfc67a7fe76dd39696b093d1b3531e4fa333678
Noah Reinthaler <noah.reinthaler@tfs-haslach.at>
2022-12-02 20:03:54 +0100

    123

--- php/ownpizza.php
+++ php/ownpizza.php
@@ -4,9 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../CSS/style.css">
+    <title>Pizza Konfigurator Ausgabe</title>
 </head>
-<body>
-    Hallo
+<body class="background">
+    <h1>Hier ist Ihre Bestellung</h1>
+    <?php
+        if(isset($_POST['senden'])){
+            $anzahl = $_POST['anzahl'];
+            $kaese = $_POST['kaese'];
+            $extra = $_POST['extra'];
+            $sausage = $_POST['sausage'];
+
+                echo "Sie haben $anzahl Pizza/en bestellt";
+
+                echo "Sie haben diese Käse Sorte/n ausgewählt: ";
+                    foreach($kaese as $key => $value){
+                        echo "$value, ";
+                    };
+
+                echo "Sie haben diese Wurst Sorte/n ausgewählt: ";
+                    foreach($sausage as $key => $value){
+                        echo "$value, ";
+                    };
+
+                echo "Sie haben diese extra Topping/s ausgewählt: ";
+                    foreach($extra as $key => $value){
+                        echo "$value, ";
+                    };
+
+        }
+        
+
+    ?>
 </body>
 </html>
